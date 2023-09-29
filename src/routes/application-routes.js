bc9b7245aedd522c18b34930ef3f6fe93eef5d3c
changwanling <wcha305@aucklanduni.ac.nz>
2023-09-30 11:14:38 +1300

    add task one

--- src/routes/application-routes.js
+++ src/routes/application-routes.js
@@ -4,7 +4,10 @@ const router = express.Router();
 
 router.get("/", function (req, res) {
   // TODO Add necessary data to res.locals before rendering the "home" page.
-
+  const allPokemons = getAllPokemon();
+  console.log(allPokemons.length);
+  //console.log(allPokemons);
+  res.locals.allPokemons = allPokemons;
   res.render("home");
 });
 
