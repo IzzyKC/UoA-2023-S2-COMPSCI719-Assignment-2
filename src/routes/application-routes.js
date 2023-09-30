80595a03dc4fd8f50335e6f41fbc78fcc79c134b
changwanling <wcha305@aucklanduni.ac.nz>
2023-09-30 23:12:15 +1300

    task two random pick favorite pokemon

--- src/routes/application-routes.js
+++ src/routes/application-routes.js
@@ -6,11 +6,11 @@ router.get("/", function (req, res) {
   // TODO Add necessary data to res.locals before rendering the "home" page.
   const allPokemons = getAllPokemon();
   console.log(allPokemons.length);
+  //all pokemon list
+  res.locals.allPokemons = allPokemons;
   //pick favorite pokemon randomly
   const randomFavoritePokemonIndex = Math.floor((Math.random() * allPokemons.length));
   console.log(allPokemons[randomFavoritePokemonIndex]);
-  //all pokemon list
-  res.locals.allPokemons = allPokemons;
   //details: favorite pokemon(randomly pick index)
   res.locals.favoritePokemon = allPokemons[randomFavoritePokemonIndex];
   //convert types array to string
