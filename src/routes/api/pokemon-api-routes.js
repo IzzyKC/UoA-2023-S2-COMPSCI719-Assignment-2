47fb1dcc27a9cfeff743bb64cbd127d1af78f35c
changwanling <wcha305@aucklanduni.ac.nz>
2023-10-01 00:11:49 +1300

    task three add get pokemon by dexnumber api

--- src/routes/api/pokemon-api-routes.js
+++ src/routes/api/pokemon-api-routes.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const { getPokemonByDexNumber } = require("../../db/pokemon-db");
 const router = express.Router();
 
 // TODO Add an API endpoint here.
 // When sending a GET request for /:dexNumber (:dexNumber is a path param),
 // return the JSON representation of the correct Pokemon, or return a 404 error
 // if that Pokemon is not found.
+router.get('/:dexNumber', function (req, res){
+    const queryDexNumber =  req.params.dexNumber;
+    console.log(queryDexNumber);
+    const pokemonData = getPokemonByDexNumber(queryDexNumber);
+    console.log(pokemonData);
+    if(pokemonData){
+        res.status(200).json(pokemonData);
+    }else{
+        res.status(404).send(`pokemon (dexNumber:${queryDexNumber}) not Found!`);
+    }
+});
 
 module.exports = router;
