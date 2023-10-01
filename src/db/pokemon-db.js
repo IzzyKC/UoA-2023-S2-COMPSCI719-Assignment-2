cadbb5b4c5ed79cca05865ee87e7f9aebd9bc068
changwanling <wcha305@aucklanduni.ac.nz>
2023-10-02 12:22:40 +1300

    task six add write json to db function

--- src/db/pokemon-db.js
+++ src/db/pokemon-db.js
@@ -1,4 +1,4 @@
-const { readJson } = require("./db.js");
+const { readJson, writeJson  } = require("./db.js");
 const path = require("path");
 
 const POKEMON_FILE_NAME = path.join(__dirname, "../json/pokemon.json");
@@ -10,6 +10,10 @@ function getAllPokemon() {
 function getPokemonByDexNumber(dexNumber) {
   const list = getAllPokemon();
   return list.find((p) => p.dexNumber == dexNumber);
+}
+
+function writePokemonJsonToFile(pokemonJson){
+  writeJson(pokemonJson, POKEMON_FILE_NAME);
 }
 
 module.exports = {
